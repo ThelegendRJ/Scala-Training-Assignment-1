5fe21a8ddb62649077e181056ffdad35c0f1efaa
TheLegendRJ <ronak.janawa@knoldus.com>
2021-10-19 18:03:27 +0530

    Assignment done

--- src/main/scala/edu/knoldus/Searching.scala
+++ src/main/scala/edu/knoldus/Searching.scala
@@ -1,9 +1,31 @@
 package edu.knoldus
+import scala.annotation.tailrec
 
 class Searching {
 
-  def binarySearch(array: Array[Int], elem: Int): Boolean = ???
+  def binarySearch(array: Array[Int], elem: Int): Boolean = {
+    def recursiveSearch(start: Int, end: Int): Boolean = {
+      if (start > end) false
+      else {
+        val mid = start + (end - start + 1) / 2
+        if (array(mid) == elem)
+          true
+        else if (elem < array(mid)) recursiveSearch(start, mid-1)
+        else recursiveSearch(mid + 1, end)
+      }
 
-  def linearSearch(array: Array[Int], elem: Int): Boolean = ???
+    }
+    recursiveSearch(0,array.length -1)
+  }
+
+
+
+
+  def linearSearch(array: Array[Int], elem: Int): Boolean = {
+
+    if (array.isEmpty) false
+    else if (array.head == elem) true
+    else linearSearch(array.tail, elem)
+  }
 
 }
--- src/main/scala/edu/knoldus/Sorting.scala
+++ src/main/scala/edu/knoldus/Sorting.scala
@@ -1,11 +1,52 @@
 package edu.knoldus
-
+import scala.util.control.Breaks.{break, breakable}
 class Sorting {
 
-  def insertionSort(array: Array[Int]): Array[Int] = ???
+  def insertionSort(array: Array[Int]): Array[Int] = {
+    for ( i <- 1 until array.length) {
+      breakable {
+        for ( j <- (1 to i).reverse) {
+          if (array(j-1) < array(j)) {
+            break
+          } else {
+            val temp = array(j)
+            array(j) = array(j-1)
+            array(j-1) = temp
+          }
+        }
+      }
+    }
+    array
+  }
 
-  def selectionSort(array: Array[Int]): Array[Int] = ???
 
-  def bubbleSort(array: Array[Int]): Array[Int] = ???
+  def selectionSort(array: Array[Int]): Array[Int] = {
+
+    def swap(arr: Array[Int], i1: Int, i2: Int) = {
+      val temp = arr(i1)
+      arr(i1) = arr(i2)
+      arr(i2) = temp }
+
+    for (i <- 0 until array.size - 1)
+      swap(array, i, (i + 1 until array.size).foldLeft(i)((currMin, index) =>
+        if (array(index) < array(currMin)) index
+        else currMin))
+    array
+  }
+
+
+  def bubbleSort(array: Array[Int]): Array[Int] = {
+    for(i<- 1 to array.length-1){
+      for(j <- (i-1) to 0 by -1){
+        if(array(j)>array(j+1)){
+          val temp=array(j+1)
+          array(j+1)=array(j)
+          array(j)=temp
+        }
+      }
+    }
+    array
+  }
+
 
 }
